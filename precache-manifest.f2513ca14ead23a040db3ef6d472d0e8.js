bac58abe69fe13e1b53c445c99283eba07725c42
PATAN KHADIR <khadir@shipnext.in>
2020-10-15 19:27:42 +0530

    Updates

---
+++ precache-manifest.f2513ca14ead23a040db3ef6d472d0e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2cbbaaef1d54539eb3b1dab9624198",
+    "url": "/time-sheet/index.html"
+  },
+  {
+    "revision": "5c6496fa7fae9c51ce24",
+    "url": "/time-sheet/static/css/main.1e6a0d1e.chunk.css"
+  },
+  {
+    "revision": "9a29da12dfc757c64f9c",
+    "url": "/time-sheet/static/js/2.0942f53a.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/time-sheet/static/js/2.0942f53a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6496fa7fae9c51ce24",
+    "url": "/time-sheet/static/js/main.8258b40e.chunk.js"
+  },
+  {
+    "revision": "a49d299768936923873f",
+    "url": "/time-sheet/static/js/runtime-main.546e7eb0.js"
+  }
+]);
